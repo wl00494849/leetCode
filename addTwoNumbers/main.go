522c8ceacfa284d610a105846090a320638b74d0
AKA迪迪弄 <wl00494849@gmail.com>
2022-02-24 16:19:33 +0800

    addTwoNumbers

--- addTwoNumbers/main.go
+++ addTwoNumbers/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type ListNode struct {
 	Val  int
 	Next *ListNode
@@ -7,8 +9,41 @@ type ListNode struct {
 
 func main() {
 
+	fmt.Scanln()
 }
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	return l1
+	newListNode := &ListNode{}
+	newListNodeHead := newListNode
+
+	for l1 != nil || l2 != nil {
+
+		if newListNode != nil {
+			if newListNode.Next == nil {
+				newListNode.Next = &ListNode{}
+			}
+		}
+
+		if l1 != nil {
+			newListNode.Next.Val += l1.Val
+			l1 = l1.Next
+		}
+
+		if l2 != nil {
+			newListNode.Next.Val += l2.Val
+			l2 = l2.Next
+		}
+
+		if newListNode.Next.Val > 9 {
+			newListNode.Next.Next = &ListNode{
+				Val: newListNode.Next.Val / 10,
+			}
+
+			newListNode.Next.Val = newListNode.Next.Val % 10
+		}
+		fmt.Println(newListNode.Val)
+		newListNode = newListNode.Next
+	}
+
+	return newListNodeHead.Next
 }
